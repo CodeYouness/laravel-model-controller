37829b86eb77fde2fb0207244825e6271440296f
CodeYouness <ylijassi@gmail.com>
2024-07-17 21:07:19 +0200

    added scss for card

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,20 +5,24 @@
 @endsection
 
 @section('main-content')
+<main id="main-app">
     <h1>
         MovieList
     </h1>
     <ul>
         @foreach ($movies as $movie)
-            <li>
-                <h3>{{$movie->title}}</h3>
-                <p>
+            <li class="card">
+                <div class="card-body">
+                <h4 class="card-title">{{$movie->title}}</h4>
+                <p class="card-subtitle text-body-secondary">
                     Date: {{$movie->date}}
                 </p>
-                <p>
+                <p class="card-text">
                     Average vote: {{$movie->vote}}
                 </p>
+                </div>
             </li>
         @endforeach
     </ul>
+</main>
 @endsection
